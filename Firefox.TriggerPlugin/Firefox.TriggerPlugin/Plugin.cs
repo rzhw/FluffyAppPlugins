a1a243b5e0458183e70137ce52e891c98cf1a47b
Richard Wang <zhwang@live.com.au>
2011-05-07 12:50:24 +1000

    Add Firefox plugin

---
+++ Firefox.TriggerPlugin/Firefox.TriggerPlugin/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NDde.Client;
+
+namespace Uploadinator.TriggerPlugins.Firefox
+{
+    [Plugin("Firefox",
+        Author = "Richard Z.H. Wang",
+        Supports = "0.10.1",
+        TriggeredBy = "firefox.exe")]
+    public class TestPlugin : IPlugin
+    {
+        public PluginResult OnTriggered()
+        {
+            DdeClient dde = new DdeClient("Firefox", "WWW_GetWindowInfo");
+            dde.Connect();
+            string result = dde.Request("URL", 1000);
+            dde.Disconnect();
+
+            var parts = result.Split(',');
+            Uri uri = new Uri(parts[0].Trim('"'));
+            return new PluginResult(uri);
+        }
+    }
+}
