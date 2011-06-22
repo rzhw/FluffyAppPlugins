818f2888aa70803f76c8496e1a82c0947fdb0046
Richard Wang <zhwang@live.com.au>
2011-06-22 21:39:30 +1000

    Bit of cleanup on the Chrome plugin

--- Chrome.TriggerPlugin/Chrome.TriggerPlugin/Plugin.cs
+++ Chrome.TriggerPlugin/Chrome.TriggerPlugin/Plugin.cs
@@ -16,7 +16,7 @@ namespace Uploadinator.TriggerPlugins.Chrome
     [Plugin("Chrome",
         Version = "0.2",
         Description = "Shorterns the URL in the currently active tab.",
-        Author = "Richard Z.H. Wang & Sebastian Müller",
+        Author = "Richard Z.H. Wang, Sebastian Müller",
         Supports = "0.10.2",
         TriggeredBy = "chrome.exe")]
     public class Plugin : IPlugin
@@ -28,8 +28,7 @@ namespace Uploadinator.TriggerPlugins.Chrome
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         static extern int GetWindowTextLength(IntPtr hWnd);
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg,
-        IntPtr wParam, StringBuilder lParam);
+        static extern IntPtr SendMessage(IntPtr hWnd, Int32 Msg, IntPtr wParam, StringBuilder lParam);
         private const int WM_GETTEXT = 0x000D;
         private const uint MAX_PATH = 255;
 
@@ -37,34 +36,28 @@ namespace Uploadinator.TriggerPlugins.Chrome
         {
             IntPtr ownerHandle = e.Handle;
             IntPtr tabHandle = FindWindowEx(ownerHandle, IntPtr.Zero, "Chrome_WidgetWin_0", null);
-            //I don't know why you used the class 'Chrome_AutocompleteEditView' first - it should be 'Chrome_OmniboxView'
             IntPtr omniboxHandle = FindWindowEx(ownerHandle, IntPtr.Zero, "Chrome_OmniboxView", null);
 
             // The tab window's title gives the page's title - perfect!
-            string tabTitle = GetTitleFromHandle(tabHandle);
+            StringBuilder sbTitle = new StringBuilder(GetWindowTextLength(tabHandle) + 1);
+            GetWindowText(tabHandle, sbTitle, sbTitle.Capacity);
+            string tabTitle = sbTitle.ToString();
 
             // Omnibox contents can be changed at any time, so it's *really* unreliable. But it's the only thing available.
-            // Also, it will drop "http://" off URLs if that's the protocol of the URL.
+            // Also, since GetWindowText won't work, we'll need to use SendMessage with WM_GETTEXT instead.
+            StringBuilder sbUrl = new StringBuilder(256);
+            SendMessage(omniboxHandle, WM_GETTEXT, (IntPtr)MAX_PATH, sbUrl);
+            string omniboxText = sbUrl.ToString().Trim(new char[] { ' ', '\0', '\n' });
 
-            // As we can't get the URL by GetTitleFromHandle, we need to use the SendMessage way using WM_GETTEXT
-            StringBuilder sb = new StringBuilder(256);
-            SendMessage(omniboxHandle, WM_GETTEXT, (IntPtr)MAX_PATH, sb);
-            string omniboxText = sb.ToString().Trim(new Char[] { ' ', '\0', '\n' });
-
+            // The Omnibox drops "http://" off URLs if that's the protocol of the URL
             if (!Uri.IsWellFormedUriString(omniboxText, UriKind.Absolute))
             {
                 omniboxText = "http://" + omniboxText;
-                if (!Uri.IsWellFormedUriString(omniboxText, UriKind.Absolute)) return null;
+                if (!Uri.IsWellFormedUriString(omniboxText, UriKind.Absolute))
+                    return null;
             }
-            return PluginResult.FromUrl(omniboxText, tabTitle);
-        }
 
-        string GetTitleFromHandle(IntPtr handle)
-        {
-            int length = GetWindowTextLength(handle);
-            StringBuilder sb = new StringBuilder(length + 1);
-            GetWindowText(handle, sb, sb.Capacity);
-            return sb.ToString();
+            return PluginResult.FromUrl(omniboxText, tabTitle);
         }
     }
 }
